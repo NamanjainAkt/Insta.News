d0d993395bc4067199ec3bd2753fab70d17d0fae
Naman jain <namanjainakt007@gmail.com>
2025-05-11 00:11:15 +0530

    fixed some bugs for production

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {React,useState} from 'react'
 import Navbar from './components/Navbar'
-import NewsBoard from './components/newsBoard'
+import NewsBoard from './components/NewsBoard'
 
 const App = () => {
   const [category, setCategory] = useState("general")
@@ -8,7 +8,6 @@ const App = () => {
     <>
     <Navbar setCategory={setCategory}/>
     <NewsBoard category={category}/>
-    
     </>
   )
 }
